cac1bb1524c13aba2e1cd7e33dcaeae02ccb573b
Sherwin Ameri <sherwinameri@gmail.com>
2017-11-06 08:11:49 -0800

    Adding revised submission

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'ui6fJWLXIu1pc3OLAZln0TPTP',
+  consumer_secret: 'c0P7emBVMpeL47P461vTzE7KFj3gw7sZ3eWy0H4DYag3RSyLSc',
+  access_token_key: '927560156329558016-QRNZ1cLo8g8zEuJ6hBh8UH0By4oLqEc',
+  access_token_secret: 'sUAhWRwFwZhbRwxfe3hpeomjdKXHWxGg3ycLPTwKDwTeA',
 }
 
 module.exports = twitterKeys;
--- liri.js
+++ liri.js
@@ -1,3 +1,5 @@
+
+  // modules
   var fs = require("fs");
   var request = require("request");
   var keys = require("./keys.js");
@@ -5,3 +7,50 @@
   var spotify = require ("spotify");
   var liriArgument = process.argv[2];
 
+
+  // commands
+  switch(liriArgument) {
+    case "my-tweets": myTweets(); break;
+    case "spotify-this-song": spotifyThisSong(); break;
+    case "movie-this": movieThis(); break;
+    case "do-what-it-says": doWhatItSays(); break;
+    // Instructions displayed in terminal to the user
+    default: console.log("\r\n" +"After typing 'node liri.js', type one of these four commands : " +"\r\n"+
+      "1. my-tweets 'enter a twitter name' " +"\r\n"+
+      "2. spotify-this-song 'enter a song name' "+"\r\n"+
+      "3. movie-this 'enter a movie name' "+"\r\n"+
+      "4. do-what-it-says.");
+  };
+
+// functions
+
+  function myTweets() {
+
+    var twitterUsername = process.argv[3];
+
+  };
+
+  function spotifyThisSong(songName) {
+
+    var songName = process.argv[3];
+
+  };
+
+  function movieThis(){
+
+    var movie = process.argv[3];
+
+  };
+
+  function doWhatItSays() {
+
+  };
+
+  // log function
+  function log(logResults) {
+    fs.appendFile("log.txt", logResults, {
+      if(error) {
+        throw error;
+      }
+    });
+  }
