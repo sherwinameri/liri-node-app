d14e97cbfc056bc34149e82c2ef62d40668deeb4
Sherwin Ameri <sherwinameri@gmail.com>
2017-11-06 07:32:39 -0800

    Submitting Files

---
+++ liri.js
@@ -0,0 +1,7 @@
+  var fs = require("fs");
+  var request = require("request");
+  var keys = require("./keys.js");
+  var twitter = require("twitter");
+  var spotify = require ("spotify");
+  var liriArgument = process.argv[2];
+
